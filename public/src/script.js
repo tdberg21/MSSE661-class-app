930369718ada06a6f0c17e7989224c640b9eeb8d
Tory Dannenberg <rfdberg@gmail.com>
2022-04-06 16:00:07 -0600

    Add login and register functionality

--- public/src/script.js
+++ public/src/script.js
@@ -1,6 +1,7 @@
-var submitButton = document.querySelector('.submit-button');
-var usernameInput = document.querySelector('.username-input');
-var passwordInput = document.querySelector('.password-input');
+var submitButton = document.querySelector(".submit-button");
+var usernameInput = document.querySelector(".username-input");
+var passwordInput = document.querySelector(".password-input");
+var emailInput = document.querySelector(".email-input");
 
 class User {
   name;
@@ -12,9 +13,43 @@ class User {
   }
 }
 
-submitButton.addEventListener('click', submitForm);
+const registerNewUser = function(event) {
+  event.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  const email = emailInput.value;
+  console.log('register')
+
+  register({
+    username: username,
+    email: email,
+    password: password
+  }).then(function(res) {
+    window.location.href = 'home.html';
+  });
+}
+
+const loginUser = function (event) {
+  event.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  console.log(username, password);
+
+  login({
+    username: username,
+    password: password
+  }).then(function(res) {
+    window.location.href = 'home.html';
+  })
+};
+
+const logoutUser = function (event) {
+  event.preventDefault();
+  console.log('logged out!');
+};
+
 
 function submitForm(event) {
   event.preventDefault();
   console.log(new User(usernameInput.value, passwordInput.value));
-};
+}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 4000;
+
 app.use(express.static('public'));
 
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/src'));
 
-app.listen(3000, () => {
-  console.log('Server started at http://localhost:%s', 3000);
+app.listen(port, () => {
+  console.log('Server started at http://localhost:%s', port);
 })
 
 
